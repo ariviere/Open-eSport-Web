e98385a2dafd78611aae4ac0d3ac456743338d67
Antoine Riviere <aereivir@gmail.com>
2013-12-04 15:46:58 +0100

    game rotation

--- js/controllers.js
+++ js/controllers.js
@@ -8,8 +8,33 @@ articleControllers.controller('ArticleListCtrl', ['$scope', '$rootScope', '$reso
 function($scope, $rootScope, $resource, $cookies, $filter) {
   
     
-    $scope.gameTitles = { 'lol': 'LEAGUE OF LEGENDS', 'sc2': 'STARCRAFT II', 'dota2': 'DOTA 2', 'csgo': 'COUNTER STRIKE : GO', 'others': 'AUTRES'};
+    $scope.games = [];
 
+    if(typeof $cookies['lolnumber'] != 'undefined')
+        $scope.games[$cookies['lolnumber']] = {'short': 'lol', 'long': 'LEAGUE OF LEGENDS'};
+    else
+        $scope.games[0] = {'short': 'lol', 'long': 'LEAGUE OF LEGENDS'};
+
+    if(typeof $cookies['sc2number'] != 'undefined')
+        $scope.games[$cookies['sc2number']] = {'short': 'sc2', 'long': 'STARCRAFT II'};
+    else
+        $scope.games[1] = {'short': 'sc2', 'long': 'STARCRAFT II'};
+    
+    if(typeof $cookies['dota2number'] != 'undefined')
+        $scope.games[$cookies['dota2number']] = {'short': 'dota2', 'long': 'DOTA 2'};
+    else
+        $scope.games[2] = {'short': 'dota2', 'long': 'DOTA 2'};
+    
+    if(typeof $cookies['csgonumber'] != 'undefined')
+        $scope.games[$cookies['csgonumber']] = {'short': 'csgo', 'long': 'COUNTER STRIKE : GO'};
+    else
+        $scope.games[3] = {'short': 'csgo', 'long': 'COUNTER STRIKE : GO'};
+    
+    if(typeof $cookies['othersnumber'] != 'undefined')
+        $scope.games[$cookies['othersnumber']] = {'short': 'others', 'long': 'AUTRES'};
+    else
+        $scope.games[4] = {'short': 'others', 'long': 'AUTRES'};
+    
     $scope.websiteIds = { 'Team aAa': 'teamaaa', 'O Gaming': 'ogaming', 'Millenium': 'millenium', 'Thunderbot': 'thunderbot', 'IEWT': 'iewt'
                         , 'VaKarM': 'vakarm', 'Reddit': 'reddit', 'onGamers': 'ongamers', 'SK Gaming': 'skgaming', 'HLTV': 'hltv', 'TeamLiquid': 'teamliquid'};
 
@@ -31,33 +56,34 @@ function($scope, $rootScope, $resource, $cookies, $filter) {
         });
         $rootScope.filterArticles();
 
-      // });
     });
     
     $rootScope.filterArticles = function(){
-        $scope.lol = [];
-        $scope.sc2 = [];
-        $scope.dota2 = [];
-        $scope.csgo = [];
-        $scope.others = [];
+        $scope.posts = {};
+
+        $scope.posts.lol = [];
+        $scope.posts.sc2 = [];
+        $scope.posts.dota2 = [];
+        $scope.posts.csgo = [];
+        $scope.posts.others = [];
 
         angular.forEach($rootScope.articles, function(article, i){
             angular.forEach($rootScope.websites, function(website, j){
                 if(article.website === website.name && (typeof $cookies[website.name] === 'undefined' || $cookies[website.name] == "true")){
                     if(article.category === "lol"){
-                        $scope.lol.push(article);
+                        $scope.posts.lol.push(article);
                     }
                     else if(article.category === "sc2"){
-                        $scope.sc2.push(article);
+                        $scope.posts.sc2.push(article);
                     }
                     else if(article.category === "dota2"){
-                        $scope.dota2.push(article);
+                        $scope.posts.dota2.push(article);
                     }
                     else if(article.category === "csgo"){
-                        $scope.csgo.push(article);
+                        $scope.posts.csgo.push(article);
                     }
                     else{
-                        $scope.others.push(article)
+                        $scope.posts.others.push(article)
                     }
                 }
             });
@@ -71,14 +97,30 @@ function($scope, $rootScope, $resource, $cookies, $filter) {
             'others': 0
         };
 
+        console.log($scope.current_page[$scope.games[0]['short']]);
         $scope.page_size = 12;
 
-        // $scope.setArticlesPage('lol');
-        // console.log("current_page: " + $scope.pagination['lol'].current_page);
+    }
 
-        //, 'sc2': 'STARCRAFT II', 'dota2': 'DOTA 2', 'csgo': 'COUNTER STRIKE : GO', 'others': 'AUTRES'};
+    $scope.move_bloc = function(left, right){
+        var temp = $scope.games[left];
+        $scope.games[left] = $scope.games[right];
+        $scope.games[right] = temp;
 
+        var indiceLeft = $scope.games[left].short + 'number';
+        var indiceRight = $scope.games[right].short + 'number';
+        $cookies[indiceLeft] = left.toString();
+        $cookies[indiceRight] = right.toString();
+    }
 
+    $scope.previousPage = function(indice){
+        console.log("previousPage " + $scope.games[indice]['short']);
+        $scope.current_page[$scope.games[indice]['short']]=$scope.current_page[$scope.games[indice]['short']]-1;
+    }
+
+    $scope.nextPage = function(indice){
+        console.log("nextPage " + $scope.games[indice]['short']);
+        $scope.current_page[$scope.games[indice]['short']]=$scope.current_page[$scope.games[indice]['short']]+1;
     }
     
     $scope.orderProp = '-pubDate';
