73450fc37b4fa48fa721a003da4f27e6f4ceaf31
Antoine Riviere <aereivir@gmail.com>
2013-12-08 16:45:32 +0100

    +esportsexpress

--- js/header.js
+++ js/header.js
@@ -5,7 +5,7 @@ headerControllers.controller('navCtrl', ['$scope', '$rootScope', '$cookies', '$r
 	
 	$scope.websiteIds = { 'Team aAa': 'teamaaa', 'O Gaming': 'ogaming', 'Millenium': 'millenium', 'Thunderbot': 'thunderbot', 'IEWT': 'iewt'
                         , 'VaKarM': 'vakarm', 'Reddit': 'reddit', 'onGamers': 'ongamers', 'SK Gaming': 'skgaming', 'HLTV': 'hltv', 'TeamLiquid': 'teamliquid'
-                        , 'joinDOTA': 'joindota', 'dota2fr': 'dota2fr', 'Esport Actu': 'esportactu' };
+                        , 'joinDOTA': 'joindota', 'dota2fr': 'dota2fr', 'Esport Actu': 'esportactu', 'Esports Express': 'esportsexpress' };
 
 	request.query(function(data){
 		$rootScope.websites = data;
